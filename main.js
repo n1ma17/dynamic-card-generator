c3822f9a235f2b7c84faad02d51b14a88d56aecd
n1ma17 <nimaa9217@gmail.com>
2022-07-29 23:14:42 +0430

    feat: card generator

---
+++ main.js
@@ -0,0 +1,22 @@
+const view = document.getElementById("view");
+
+function cardGenerator(m, n) {
+  const numbers = Array.from({ length: m * n }, (_, i) => i + 1);
+  for (let i = 0; i < m; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    view.appendChild(row);
+    for (let j = 0; j < n; j++) {
+      const cardNumber = numbers.splice(
+        Math.floor(Math.random() * numbers.length),
+        1
+      );
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.innerText = cardNumber;
+      row.appendChild(card);
+    }
+  }
+}
+
+cardGenerator(6, 4);
