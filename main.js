cdf2109dd160ab6c37e6a8c527a41f6aaf90642b
n1ma17 <nimaa9217@gmail.com>
2022-07-29 23:26:54 +0430

    fix: enteries guard

--- main.js
+++ main.js
@@ -2,21 +2,23 @@ const view = document.getElementById("view");
 
 function cardGenerator(m, n) {
   const numbers = Array.from({ length: m * n }, (_, i) => i + 1);
-  for (let i = 0; i < m; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    view.appendChild(row);
-    for (let j = 0; j < n; j++) {
-      const cardNumber = numbers.splice(
-        Math.floor(Math.random() * numbers.length),
-        1
-      );
-      const card = document.createElement("div");
-      card.classList.add("card");
-      card.innerText = cardNumber;
-      row.appendChild(card);
+  if (m && n) {
+    for (let i = 0; i < m; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      view.appendChild(row);
+      for (let j = 0; j < n; j++) {
+        const cardNumber = numbers.splice(
+          Math.floor(Math.random() * numbers.length),
+          1
+        );
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerText = cardNumber;
+        row.appendChild(card);
+      }
     }
-  }
+  }else alert('please insert rows and columns')
 }
 
 cardGenerator(6, 4);
